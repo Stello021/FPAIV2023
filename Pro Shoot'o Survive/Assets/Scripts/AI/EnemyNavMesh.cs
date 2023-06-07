21e9a294d8b37c680e032130f8a6afbdec74e431
Stello021 <101042111+Stello021@users.noreply.github.com>
2023-06-07 18:22:07 +0200

    Associate Agent Speed to Animator Speed

--- Pro Shoot'o Survive/Assets/Scripts/AI/EnemyNavMesh.cs
+++ Pro Shoot'o Survive/Assets/Scripts/AI/EnemyNavMesh.cs
@@ -6,7 +6,9 @@ using UnityEngine.AI;
 public class EnemyNavMesh : MonoBehaviour
 {
     [SerializeField] private Transform destinantionTransform; //enemy destination transform
+    [SerializeField] private Animator enemyAnimator;
     private NavMeshAgent enemyAgent;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,8 @@ public class EnemyNavMesh : MonoBehaviour
     private void Awake()
     {
         enemyAgent = GetComponent<NavMeshAgent>();
+        enemyAnimator = GetComponent<Animator>();
+        enemyAnimator.speed = enemyAgent.speed;
     }
     // Update is called once per frame
     void Update()
