a62209222c0eca7858190fc8eec5e73d61926ca3
Stello021 <101042111+Stello021@users.noreply.github.com>
2023-06-09 11:28:15 +0200

    Fix Ranged Enemy

--- Pro Shoot'o Survive/Assets/Scripts/Enemies/AI/RangedEnemyAI.cs
+++ Pro Shoot'o Survive/Assets/Scripts/Enemies/AI/RangedEnemyAI.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Animations;
 
 public class RangedEnemyAI : EnemyAI
 {
-    [SerializeField] LayerMask whatIsPlayer;
+    [SerializeField] LayerMask PlayerMask; //Layer mask to recognize Player Layer
     [SerializeField] GameObject bulletPrefab;
     [SerializeField] GameObject OwnWeapon;
-    public Transform BulletStartingPoint;
+    public Transform BulletStartingPoint; //Bullets Spawn Point
     private float startingEnemySpeed;
-
     //Shooting
     public float TimeBetweenShoots;
     private bool alreadyShooted;
@@ -21,56 +21,59 @@ public class RangedEnemyAI : EnemyAI
     {
         enemyAgent = GetComponent<NavMeshAgent>();
         enemyAnimator = GetComponent<Animator>();
-        enemyAgent.stoppingDistance = ShootRange;
         startingEnemySpeed = enemyAgent.speed;
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Check for shoot range
-        playerInShootRange = Physics.CheckSphere(transform.position, ShootRange, whatIsPlayer);
+        //Check for shoot range, range is the ray of sphere with origin on enemy position 
+        playerInShootRange = Physics.CheckSphere(transform.position, ShootRange, PlayerMask);
+        enemyAnimator.SetFloat("Speed_f", enemyAgent.speed);//Associate animator's speed parameter with agent speed
+        //FSM
         if (!playerInShootRange)
         {
-            OwnWeapon.SetActive(false);
-            enemyAgent.speed = startingEnemySpeed;
-            enemyAnimator.SetInteger("WeaponType_int", 0);
-            enemyAnimator.SetBool("Shoot_b", false);
-            enemyAgent.SetDestination(PlayerTransform.position);
-
+            ChasePlayer();
         }
         else
         {
             ShootPlayer();
         }
-        enemyAnimator.SetFloat("Speed_f", enemyAgent.speed);
+
+    }
+    private void ChasePlayer()
+    {
+        OwnWeapon.SetActive(false); //Deactivate Weapon
+        enemyAgent.speed = startingEnemySpeed; 
+        enemyAgent.SetDestination(PlayerTransform.position);
+        //Animation settings
+        enemyAnimator.SetInteger("WeaponType_int", 0);
+        enemyAnimator.SetBool("Shoot_b", false);
 
     }
     private void ShootPlayer()
     {
-        enemyAgent.SetDestination(transform.position);
+        gameObject.transform.LookAt(new Vector3(PlayerTransform.position.x, 0, PlayerTransform.position.z));
         enemyAgent.speed = 0;
+        enemyAgent.SetDestination(transform.position);
         if (!alreadyShooted)
         {
             enemyAnimator.SetInteger("WeaponType_int", 1);
             enemyAnimator.SetBool("Shoot_b", true);
             OwnWeapon.SetActive(true);
-            Instantiate(bulletPrefab, BulletStartingPoint.position, BulletStartingPoint.rotation);
+            Invoke(nameof(SpawnBullet), TimeBetweenShoots);
             alreadyShooted = true;
             Invoke(nameof(ResetAttack), TimeBetweenShoots);
             
         }
     }
+    private void SpawnBullet()
+    {
+        Instantiate(bulletPrefab, BulletStartingPoint.position, BulletStartingPoint.rotation);
+    }
     private void ResetAttack()
     {
         alreadyShooted = false;
     }
-    private void Animations()
-    {
-    }
+
 }
