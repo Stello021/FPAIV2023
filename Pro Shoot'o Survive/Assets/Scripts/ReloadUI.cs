782d409f3c5e3316f2c0f64888d019411e199033
GiulioLV98 <giulioleoni5@gmail.com>
2023-06-18 16:50:43 +0200

    Reload UI

---
+++ Pro Shoot'o Survive/Assets/Scripts/ReloadUI.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReloadUI : MonoBehaviour
+{
+    [SerializeField] Image image;
+    [SerializeField] float reloadTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (image.fillAmount < 1)
+        {
+            image.fillAmount += 1f / reloadTime * Time.deltaTime;
+
+            if (image.fillAmount >= 1)
+            {
+                gameObject.SetActive(false);
+                Debug.Log(Time.time);
+            }
+        }
+    }
+}
