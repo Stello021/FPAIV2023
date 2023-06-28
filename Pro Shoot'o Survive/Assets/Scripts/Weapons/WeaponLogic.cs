76a8662e6ca0ad857640a8cb1290a09ee41af7e5
Cerz1 <alessiodiiorios5@gmail.com>
2023-06-28 05:51:09 +0200

    new weapon logic

--- Pro Shoot'o Survive/Assets/Scripts/Weapons/WeaponLogic.cs
+++ Pro Shoot'o Survive/Assets/Scripts/Weapons/WeaponLogic.cs
@@ -1,19 +1,101 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponLogic : MonoBehaviour
 {
-    public float Damage;
-    // Start is called before the first frame update
-    void Start()
+    public int bulletsPerMagazine;  // Total bullets per magazine
+    public int totalMagazines;      // Total number of magazines
+    public float fireRate;          // Fire rate in rounds per second
+    public Transform bulletSpawnPoint;  // Point where bullets are instantiated
+    public GameObject bulletPrefab;  // Prefab of the bullet object
+
+    protected int bulletsRemainingInMagazine;  // Bullets remaining in the current magazine
+    protected bool isReloading;  // Flag to indicate if the weapon is currently reloading
+    protected float fireTimer;   // Timer to control the fire rate
+
+    protected float Damage;  // Damage caused by the bullet
+
+    protected virtual void Start()
     {
+        bulletsRemainingInMagazine = bulletsPerMagazine;
+        isReloading = false;
+
+        // Assign the "Weapon" tag to the game object
         gameObject.tag = "Weapon";
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
+        // Decrement the fire timer
+        if (fireTimer > 0f)
+        {
+            fireTimer -= Time.deltaTime;
+        }
+    }
 
+    public virtual void Fire()
+    {
+        // If the weapon is reloading, don't allow shooting
+        if (isReloading)
+        {
+            return;
+        }
+
+        // Check if there are bullets remaining in the magazine
+        if (bulletsRemainingInMagazine > 0)
+        {
+            // Instantiate a bullet at the bullet spawn point
+            GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+
+            // Get the PlayerBullet script component from the bullet object
+            PlayerBullet bulletScript = bullet.GetComponent<PlayerBullet>();
+            if (bulletScript != null)
+            {
+                // Set the bullet damage in the PlayerBullet script
+                bulletScript.DamageDealt = Damage;
+            }
+
+            // Decrement the bullets remaining in the magazine
+            bulletsRemainingInMagazine--;
+
+            // Reset the fire timer
+            fireTimer = 1f / fireRate;
+        }
+        else
+        {
+            // Out of bullets, need to reload
+            Reload();
+        }
+    }
+
+    protected virtual void Reload()
+    {
+        // Check if there are additional magazines available
+        if (totalMagazines > 0)
+        {
+            isReloading = true;
+
+            // Perform the reloading animation or any other actions
+
+            // Delay the reloading process
+            float reloadTime = 2f;  // You can adjust the reload time based on your requirements
+            Invoke("FinishReloading", reloadTime);
+        }
+        else
+        {
+            // No more magazines, weapon is empty
+            Debug.Log("Weapon is empty!");
+        }
+    }
+
+    protected virtual void FinishReloading()
+    {
+        // Refill the magazine with bullets
+        bulletsRemainingInMagazine = bulletsPerMagazine;
+
+        // Decrement the total number of magazines
+        totalMagazines--;
+
+        isReloading = false;
     }
 }
+
