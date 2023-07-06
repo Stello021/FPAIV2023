3122a40410e233e251855f8dd36b21683713e13b
GiulioLV98 <giulioleoni5@gmail.com>
2023-07-06 17:39:23 +0200

    Homing nerfed

--- Pro Shoot'o Survive/Assets/Scripts/Player/PlayerLogic.cs
+++ Pro Shoot'o Survive/Assets/Scripts/Player/PlayerLogic.cs
@@ -17,7 +17,6 @@ public class PlayerLogic : MonoBehaviour
     [SerializeField] private float armorMax;
     public float Armor { get { return armor; } set { armor = Mathf.Clamp(value, 0, armorMax); UpdateArmorText(); } }
 
-
     public float ReloadMultiplier = 1f;
 
     public float speedMultiplier = 1f;
